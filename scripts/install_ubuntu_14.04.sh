ff50066667731a532036ca4c6ba3e8cb6cb9f600
Ben Christensen <benjchristensen@gmail.com>
2016-05-17 13:41:07 -0700

    Create install_ubuntu_14.04.sh

---
+++ scripts/install_ubuntu_14.04.sh
@@ -0,0 +1,13 @@
+# store binary on Github
+git config --global user.name "Travis Git"
+git config --global user.email "benjchristensen+travisgit@gmail.com"
+git config --global push.default simple
+echo "Username is: $GITHUB_USERNAME"
+git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ReactiveSocket/reactivesocket-cpp-dependencies.git
+cd reactivesocket-cpp-dependencies
+cd lib
+touch junk
+git add junk
+git commit -a -m "folly binary"
+git push origin
+cd ..
