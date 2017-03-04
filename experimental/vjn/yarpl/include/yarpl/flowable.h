90a62b7beb6165ff75c077396c66df7f2c2662a3
vjn <vijayan.rajan@gmail.com>
2017-03-04 08:49:23 -0800

    vjn

--- experimental/vjn/yarpl/include/yarpl/flowable.h
+++ experimental/vjn/yarpl/include/yarpl/flowable.h
@@ -22,16 +22,12 @@ public:
   template<typename F, typename = typename std::enable_if<
       std::is_callable<F(Subscriber&), void>::value>::type>
   static std::shared_ptr<Flowable> create(F&& function) {
-    return std::shared_ptr<Flowable>(
-        new Derived<F>(std::forward<F>(function)));
+    return std::make_shared<Derived<F>>(std::forward<F>(function));
   }
 
   template<typename F, typename = typename std::enable_if<
       std::is_callable<F(T), typename std::result_of<F(T)>::type>::value>::type>
-  auto map(F&& function) {
-    return std::shared_ptr<Flowable<typename std::result_of<F(T)>::type>>(
-        new Mapper<F>(std::forward<F>(function), this->shared_from_this()));
-  }
+  auto map(F&& function);
 
   virtual void subscribe_on(std::unique_ptr<Subscriber> subscriber,
       Scheduler& scheduler) = 0;
@@ -69,58 +65,19 @@ private:
   private:
     const std::shared_ptr<Function> function_;
   };
-
-  template<typename Transform>
-  class Mapper : public Flowable, public Subscriber {
-  public:
-    Mapper(Transform&& transform, std::shared_ptr<Flowable<T>> upstream)
-        : forwarder_(std::unique_ptr<Forwarder>(
-            new Forwarder(std::forward<Transform>(transform)))),
-          upstream_(upstream) {}
-
-    void subscribe(std::unique_ptr<Subscriber> subscriber) override {
-      // TODO(vjn): check that subscriber_.get() == nullptr?
-      forwarder_->set_subscriber(std::move(subscriber));
-      upstream_->subscribe(std::move(forwarder_));
-    }
-
-    virtual void subscribe_on(std::unique_ptr<Subscriber> subscriber,
-        Scheduler& scheduler) override {
-    }
-  private:
-    class Forwarder : public Subscriber {
-    public:
-      Forwarder(Transform&& transform)
-          : transform_(std::forward<Transform>(transform)) {}
-
-      void set_subscriber(std::unique_ptr<Subscriber> subscriber) {
-        subscriber_ = std::move(subscriber);
-      }
-
-      virtual void on_next(const T& value) override {
-        subscriber_->on_next(transform_(value));
-      }
-
-      virtual void on_error(const std::exception& error) override {
-        subscriber_->on_error(error);
-      }
-
-      virtual void on_complete() override {
-        subscriber_->on_complete();
-      }
-
-      virtual void on_subscribe(Subscription* subscription) override {
-        subscriber_->on_subscribe(subscription);
-      }
-
-    private:
-      Transform transform_;
-      std::unique_ptr<Subscriber> subscriber_;
-    };
-
-    std::unique_ptr<Forwarder> forwarder_;
-    const std::shared_ptr<Flowable<T>> upstream_;
-  };
 };
+
+}  // yarpl
+
+#include "yarpl/operators/map.h"
+
+namespace yarpl {
+
+template<typename T>
+template<typename F, typename Default>
+auto Flowable<T>::map(F&& function) {
+  return std::make_shared<operators::Mapper<T, F>>(
+      std::forward<F>(function), this->shared_from_this());
+}
 
 }  // yarpl
