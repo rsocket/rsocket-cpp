186eefa115301b9bc3489083e51d8e968ba5b7cd
Melissa Winstanley <mwinst@mwinst-mbp.dhcp.thefacebook.com>
2017-05-01 14:23:45 -0700

    Refactor RSocketConnectionHandler out of RSocketServer

--- experimental/rsocket/RSocketServer.h
+++ experimental/rsocket/RSocketServer.h
@@ -8,12 +8,10 @@
 #include <folly/Synchronized.h>
 
 #include "rsocket/ConnectionAcceptor.h"
-#include "rsocket/ConnectionResumeRequest.h"
 #include "rsocket/ConnectionSetupRequest.h"
 #include "rsocket/RSocketRequestHandler.h"
-
-#include "src/ServerConnectionAcceptor.h"
 #include "src/ReactiveSocket.h"
+#include "src/ServerConnectionAcceptor.h"
 
 namespace rsocket {
 
@@ -71,6 +69,8 @@ class RSocketServer {
   // TODO version supporting Stats and other params
   // RSocketServer::start(OnAccept onAccept, ServerSetup setupParams)
 
+  friend class RSocketServerConnectionHandler;
+
  private:
   void addSocket(std::unique_ptr<reactivesocket::ReactiveSocket>);
   void removeSocket(reactivesocket::ReactiveSocket*);
@@ -90,4 +90,4 @@ class RSocketServer {
   folly::Baton<> waiting_;
   folly::Optional<folly::Baton<>> shutdown_;
 };
-}
+} // namespace rsocket
