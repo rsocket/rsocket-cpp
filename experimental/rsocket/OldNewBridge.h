e8be0186a01927f8461c72a2380ecd945f872bc4
Nick Gibson <nickg@fb.com>
2017-05-02 14:48:41 -0700

    Fix nit

--- experimental/rsocket/OldNewBridge.h
+++ experimental/rsocket/OldNewBridge.h
@@ -121,8 +121,7 @@ class NewToOldSubscriber : public yarpl::flowable::Subscriber<reactivesocket::Pa
   void onNext(const reactivesocket::Payload& payload) override {
     // Cloning IOBufs just shares their internal buffers, so this isn't the end
     // of the world.
-    auto clone = payload.clone();
-    inner_->onNext(std::move(clone));
+    inner_->onNext(payload.clone());
   }
 
   void onComplete() override {
