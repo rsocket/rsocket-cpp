31103d6ec3ab9d3f646df7699a08384ec35d8280
Shai Szulanski <iahs@fb.com>
2020-02-24 13:41:39 -0800

    Add ServerStream adaptor for Flowable
    Reviewed By: rhodo
    
    Differential Revision: D20035244
    
    fbshipit-source-id: 1d5763ee3daf41fae814bf95728a8db8664a9a69

--- yarpl/flowable/ThriftStreamShim.h
+++ yarpl/flowable/ThriftStreamShim.h
@@ -1,20 +1,25 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
 #pragma once
-#if FOLLY_HAS_COROUTINES
 
+#include <folly/Portability.h>
+#if FOLLY_HAS_COROUTINES
 #include <folly/experimental/coro/Baton.h>
 #include <folly/experimental/coro/Invoke.h>
 #include <folly/experimental/coro/Task.h>
+#endif
+
 #include <thrift/lib/cpp2/async/ClientBufferedStream.h>
+#include <thrift/lib/cpp2/async/ServerStream.h>
 #include <yarpl/flowable/Flowable.h>
 
 namespace yarpl {
 namespace flowable {
-class ClientBufferedStreamShim {
+class ThriftStreamShim {
  public:
+#if FOLLY_HAS_COROUTINES
   template <typename T>
-  static std::shared_ptr<yarpl::flowable::Flowable<T>> create(
+  static std::shared_ptr<yarpl::flowable::Flowable<T>> fromClientStream(
       apache::thrift::ClientBufferedStream<T>&& stream,
       folly::Executor::KeepAlive<folly::SequencedExecutor> ex) {
     struct SharedState {
@@ -138,8 +143,115 @@ class ClientBufferedStreamShim {
               .start();
         });
   }
+#endif
+
+  template <typename T>
+  static apache::thrift::ServerStream<T> toServerStream(
+      std::shared_ptr<Flowable<T>> flowable) {
+    class StreamServerCallbackAdaptor final
+        : public apache::thrift::StreamServerCallback,
+          public Subscriber<T> {
+     public:
+      explicit StreamServerCallbackAdaptor(
+          folly::Try<apache::thrift::StreamPayload> (*encode)(folly::Try<T>&&),
+          folly::EventBase* eb)
+          : encode_(encode), eb_(eb) {}
+      // StreamServerCallback implementation
+      bool onStreamRequestN(uint64_t tokens) override {
+        if (!subscription_) {
+          tokensBeforeSubscribe_ += tokens;
+        } else {
+          DCHECK_EQ(0, tokensBeforeSubscribe_);
+          subscription_->request(tokens);
+        }
+        return clientCallback_;
+      }
+      void onStreamCancel() override {
+        clientCallback_ = nullptr;
+        if (auto subscription = std::move(subscription_)) {
+          subscription->cancel();
+        }
+        self_.reset();
+      }
+      void resetClientCallback(
+          apache::thrift::StreamClientCallback& clientCallback) override {
+        clientCallback_ = &clientCallback;
+      }
+
+      // Subscriber implementation
+      void onSubscribe(std::shared_ptr<Subscription> subscription) override {
+        eb_->add([this, subscription = std::move(subscription)] {
+          if (!clientCallback_) {
+            return subscription->cancel();
+          }
+
+          subscription_ = std::move(subscription);
+          if (auto tokens = std::exchange(tokensBeforeSubscribe_, 0)) {
+            subscription_->request(tokens);
+          }
+        });
+      }
+      void onNext(T next) override {
+        eb_->add([this, next = std::move(next), s = self_] {
+          if (clientCallback_) {
+            std::ignore =
+                clientCallback_->onStreamNext(apache::thrift::StreamPayload{
+                    encode_(folly::Try<T>(std::move(next))).value().payload,
+                    {}});
+          }
+        });
+      }
+      void onError(folly::exception_wrapper ew) override {
+        eb_->add([this, ew = std::move(ew), s = self_] {
+          if (clientCallback_) {
+            std::exchange(clientCallback_, nullptr)
+                ->onStreamError(
+                    encode_(folly::Try<T>(std::move(ew))).exception());
+            self_.reset();
+          }
+        });
+      }
+      void onComplete() override {
+        eb_->add([this, s = self_] {
+          if (clientCallback_) {
+            std::exchange(clientCallback_, nullptr)->onStreamComplete();
+            self_.reset();
+          }
+        });
+      }
+
+      void takeRef(std::shared_ptr<StreamServerCallbackAdaptor> self) {
+        self_ = std::move(self);
+      }
+
+     private:
+      apache::thrift::StreamClientCallback* clientCallback_{nullptr};
+      std::shared_ptr<Subscription> subscription_;
+      uint32_t tokensBeforeSubscribe_{0};
+      folly::Try<apache::thrift::StreamPayload> (*encode_)(folly::Try<T>&&);
+      folly::EventBase* eb_;
+      std::shared_ptr<StreamServerCallbackAdaptor> self_;
+    };
+
+    return apache::thrift::ServerStream<T>(
+        [flowable = std::move(flowable)](
+            folly::Executor::KeepAlive<>,
+            folly::Try<apache::thrift::StreamPayload> (*encode)(
+                folly::Try<T> &&)) mutable {
+          return [flowable = std::move(flowable), encode](
+                     apache::thrift::FirstResponsePayload&& payload,
+                     apache::thrift::StreamClientCallback* callback,
+                     folly::EventBase* clientEb) mutable {
+            auto stream =
+                std::make_shared<StreamServerCallbackAdaptor>(encode, clientEb);
+            stream->takeRef(stream);
+            stream->resetClientCallback(*callback);
+            std::ignore = callback->onFirstResponse(
+                std::move(payload), clientEb, stream.get());
+            flowable->subscribe(std::move(stream));
+          };
+        });
+  }
 };
 } // namespace flowable
 } // namespace yarpl
-
-#endif
